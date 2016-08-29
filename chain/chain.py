cf24f8c3658924baefef0efee60e4f52fa9e191e
Jacob Meacham <jacob.e.meacham@gmail.com>
2016-08-29 11:43:38 -0700

    Add new and add commands

--- chain/chain.py
+++ chain/chain.py
@@ -1,9 +1,98 @@
+import json
+from enum import Enum
+
+import os
+import time
+
+
+class Frequency(Enum):
+    daily = 1
+    weekly = 2
+    monthly = 3
+
+
+class ChainExistsException(Exception):
+    def __init__(self, name):
+        self.message = 'Chain {} already exists'.format(name)
+
+
+class NoChainExistsException(Exception):
+    def __init__(self, name):
+        self.message = 'No Chain named {} exists'.format(name)
+
+
+def _safe_makedirs(dir_name):
+    """Make a directory if it doesn't already exist.
+
+    Args:
+        dir_name: The directory to make.
+
+    """
+    try:
+        os.makedirs(dir_name)
+    except OSError:
+        if not os.path.isdir(dir_name):
+            raise
+
+
+def _ensure_chain_file(file_path):
+    if not os.path.exists(file_path):
+        _safe_makedirs(os.path.dirname(file_path))
+
+        with open(file_path, 'x') as f:
+            json.dump([], f)
+
+
 class ChainClient(object):
-    def __init__(self, chain_file):
-        self._chain_file = chain_file
+    def __init__(self, chain_file_path):
+        self._chain_file_path = os.path.expanduser(chain_file_path)
+
+        _ensure_chain_file(self._chain_file_path)
+
+    def _update_chains(self, chains):
+        with open(self._chain_file_path, 'w') as f:
+            json.dump(chains, f, indent=4, separators=[',', ': '])
 
     def new_chain(self, name, frequency, description, num_required):
-        pass
+        with open(self._chain_file_path, 'r') as f:
+            chains = json.load(f)
 
-    def add_link_to_chain(self, name, number, message):
-        pass
+        if len([c for c in chains if c['name'].lower() == name.lower()]) > 0:
+            # Chain already exists
+            raise ChainExistsException(name)
+
+        chain = {
+            "name": name,
+            "frequency": frequency.name,
+            "description": description,
+            "required": num_required,
+            "links": []
+        }
+
+        chains.append(chain)
+
+        self._update_chains(chains)
+        return chain
+
+    def add_link_to_chain(self, name, number=1, message=''):
+        with open(self._chain_file_path, 'r') as f:
+            chains = json.load(f)
+
+        # Find the correct chain
+        chain_to_update = [(i, c) for i, c in enumerate(chains) if c['name'].lower() == name.lower()]
+        if len(chain_to_update) == 0:
+            raise NoChainExistsException(name)
+
+        # Just take the first chain, if there are multiple.
+        index, updated_chain = chain_to_update[0]
+        updated_chain['links'].append({
+            # TODO: Fix this
+            'timestamp': time.time(),
+            'number': number,
+            'message': message
+        })
+
+        chains[index] = updated_chain
+
+        self._update_chains(chains)
+        return updated_chain
--- chain/cli.py
+++ chain/cli.py
@@ -1,33 +1,63 @@
 import click
 
-from chain.chain import ChainClient
+from chain.chain import ChainClient, Frequency, NoChainExistsException, ChainExistsException
 
-DEFAULT_DATA_PATH='~/.chain/chains.json'
+DEFAULT_DATA_PATH = '~/.chain/chains.json'
 
 # This is idiomatic for click
 # pylint: disable=C0103
 pass_chain_context = click.make_pass_decorator(ChainClient)
 
+
 @click.group()
-@click.option('--file', metavar='FILE', help='Data file, default is ~/.chain/chains.json', type=click.Path(), default=DEFAULT_DATA_PATH)
+@click.option('--file', metavar='FILE', help='Data file path, default is ~/.chain/chains.json', type=click.Path(),
+              default=DEFAULT_DATA_PATH)
 @click.version_option('0.0.1')
 @click.pass_context
 def cli(ctx, file):
     ctx.obj = ChainClient(file)
 
+
 @cli.command(name='new', help='add a new chain')
-@click.argument('name', help='name of the chain')
-@click.option('--daily', is_flag=True, help='Add daily links', default=True)
+@click.argument('name')
+@click.option('--daily', is_flag=True, help='Add daily links')
 @click.option('--weekly', is_flag=True, help='Add weekly links')
 @click.option('--monthly', is_flag=True, help='Add monthly links')
-@click.option('--required', help='Number of links required for the chain to be considered unbroken')
-@click.option('--description', help='Description of this chain')
+@click.option('--required', help='Number of links required for the chain to be considered unbroken', default=1)
+@click.option('--description', help='Description of this chain', default='')
 @pass_chain_context
 def new_chain(client, name, daily, weekly, monthly, required, description):
-    client.new_chain(name, frequency=0, description=description, num_required=required)
-    click.echo("New chain {} created. Don't break the chain!".format(name))
+    frequency = Frequency.daily
+    if [daily, weekly, monthly].count(True) > 1:
+        raise click.BadArgumentUsage('One and only one of --daily, --weekly, --monthly must be set.')
+
+    if weekly:
+        frequency = Frequency.weekly
+    elif monthly:
+        frequency = Frequency.monthly
+
+    try:
+        client.new_chain(name, frequency=frequency, description=description, num_required=required)
+    except ChainExistsException as e:
+        raise click.BadArgumentUsage(e.message)
+
+    click.echo("New chain \"{}\" created. Don't break the chain!".format(name))
+
 
 @cli.command(name='add', help='add a link to the chain')
-@click.argument('name', help='name of the chain')
-@click.argument('name', help='name of the chain')
-@click.option('--message', '-m', help='Number of links required for the chain to be considered unbroken')
+@click.argument('name')
+@click.option('--num', help='Number of links to add', default=1)
+@click.option('--message', '-m', help='Message attached to the added link', default='')
+@pass_chain_context
+def add_link(client, name, num, message):
+    try:
+        client.add_link_to_chain(name, num, message=message)
+    except NoChainExistsException as e:
+        raise click.BadArgumentUsage(e.message)
+
+    click.echo("Added {} links to chain \"{}\". Don't break the chain!".format(num, name))
+
+
+if __name__ == '__main__':
+    # pylint: disable=E1120
+    cli()
